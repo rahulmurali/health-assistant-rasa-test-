85602026becf359e8441779e29773676615cb0b0
rahulmurali <rahulmurali07@gmail.com>
2021-03-06 23:36:52 +0530

    actions added to actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,58 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class HealthForm(FormAction):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self):
+        return "health_form"
+
+    @staticmethod
+    def required_slots(tracker):
+
+        if tracker.get_slot('confirm_exercise') == "yes":
+            return ["confirm_exercise", "exercise", "sleep",
+                    "diet", "stress", "goal"]
+        else:
+            return ["confirm_exercise", "sleep",
+                    "diet", "stress", "goal"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "confirm_exercise": [
+                self.from_intent(intent="affirm", value="yes"),
+                self.from_intent(intent="deny", value="no"),
+                self.from_intent(intent="inform", value="yes"),
+            ],
+            "sleep": [
+                self.from_entity(entity="sleep"),
+                self.from_intent(intent="deny", value="None"),
+            ],
+            "diet": [
+                self.from_text(intent="inform"),
+                self.from_text(intent="affirm"),
+                self.from_text(intent="deny"),
+            ],
+            "goal": [
+                self.from_text(intent="inform"),
+            ],
+        }
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        dispatcher.utter_message("Thanks, great job!")
+        return []
